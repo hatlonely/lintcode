75d5ef18731a390cde030b396cf533ab01e826c6
hatlonely <hatlonely@foxmail.com>
2015-06-22 22:11:30 +0800

    add compare strings

--- TwoStringsAreAnagrams.java
+++ TwoStringsAreAnagrams.java
@@ -1,6 +1,13 @@
-// 两个字符串是变位词
+// Two Strings Are Anagrams
 // http://www.lintcode.com/zh-cn/problem/two-strings-are-anagrams/
-// 写出一个函数 anagram(source, target) 去判断两个字符串是否是颠倒字母顺序构成的
+//
+// Write a method anagram(s,t) to decide if two strings are anagrams or not.
+//
+// Example
+// Given s="abcd", t="dcab", return true.
+//
+// Challenge
+// O(n) time, O(1) extra space
 
 package lintcode;
 
@@ -11,10 +18,8 @@ public class TwoStringsAreAnagrams {
     private int[] targetTimes = new int[256];
 
     /**
-     * @param source
-     *            : The first string
-     * @param target
-     *            : The second string
+     * @param source The first string
+     * @param target The second string
      * @return true or false
      */
     public boolean anagram(String source, String target) {
